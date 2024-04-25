e1fe48297487411f5ee41e34ca76d118996a42c5
nmatos <nmatos@pursuit.org>
2024-04-25 13:14:30 -0400

    used useEffect to change order of cohortList

--- src/cohortList/CohortList.jsx
+++ src/cohortList/CohortList.jsx
@@ -1,17 +1,31 @@
 import './cohortList.scss';
 import React from 'react';
+import { useEffect } from 'react';
 import { useState } from 'react';
 
+
 const CohortList = ({ data, setSelectCohort }) => {
-  const cohorts = [...new Set(data.map(cohort => cohort.cohort.cohortCode))];
+  const [cohort, setCohort] = useState([]);
+
+
+  // useEffect hook changes the cohort state with unique and sorted cohort codes whenever data( the prop) changes or updates. Also known as a side effect.
+  useEffect(() => {
+    const newCohortListOrder = ['Winter', 'Fall', 'Summer', 'Spring'];
+    setCohort([...new Set(data.map(c => c.cohort.cohortCode))].sort((a,b) => {
+      const [seasonA, yearA] = a.split(/(\d+)/);
+      const [seasonB, yearB] = b.split(/(\d+)/);
+      return yearB - yearA || newCohortListOrder.indexOf(seasonA) - newCohortListOrder.indexOf(seasonB)
+    }));
+  }, [data])
 
   const handleCohortClick = (cohort) => {
     setSelectCohort(cohort);
   }
+    
 
   return (
     <div className='cohortList'>
-      {cohorts.map((cohort, index) => (
+      {cohort.map((cohort, index) => (
         <div className="cohortList__cohorts" key={index} onClick={() => handleCohortClick(cohort)}>
           <h2 className='cohortList__cohort'>{cohort}</h2>
         </div>
